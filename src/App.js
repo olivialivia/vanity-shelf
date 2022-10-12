afab5a0f436078f06cdfcdefaa34547dd0d9b168
olivialivia <94136530+olivialivia@users.noreply.github.com>
2022-10-12 14:53:09 +0100

    set up files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Vanity Shelf</h1>
+        </div>
+    );
+}
+
+export default App;
+
+/* 
+
+shelf #1: make up
+shelf #2: skincare
+shelf #3: hair
+shelf #4: other (hand creams etc)
+
+*/
